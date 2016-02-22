29fc78f83f769649ede96e4d9c0aaa3df52799ab
wypa4ok <wupa4ok@gmail.com>
2016-02-22 18:12:58 -0500

    Initaial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var app = angular.module('MobileAngularUiExamples', [
+  'ngRoute',
+  'mobile-angular-ui',
+  
+  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+  // it is at a very beginning stage, so please be careful if you like to use
+  // in production. This is intended to provide a flexible, integrated and and 
+  // easy to use alternative to other 3rd party libs like hammer.js, with the
+  // final pourpose to integrate gestures into default ui interactions like 
+  // opening sidebars, turning switches on/off ..
+  'mobile-angular-ui.gestures'
+]);
+
+app.run(function($transform) {
+  window.$transform = $transform;
+});
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {controller:'gameController', templateUrl: 'views/game/game.html', reloadOnSearch: false});
+  //$routeProvider.when('/game', {controller:'scrGameController', templateUrl: 'game/scrGame/scrGame.html', reloadOnSearch: false}); 
+});
+
+app.controller('MainController', function($rootScope, $scope){
+
+  $scope.swiped = function(direction) {
+    alert('Swiped ' + direction);
+  };
+
+  // User agent displayed in home page
+  $scope.userAgent = navigator.userAgent;
+  
+  // Needed for the loading screen
+  $rootScope.$on('$routeChangeStart', function(){
+    $rootScope.loading = true;
+  });
+
+  $rootScope.$on('$routeChangeSuccess', function(){
+    $rootScope.loading = false;
+  });
+    
+});
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+<!--        <base href="/demo/" />-->
+        <title>Barley-Break</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimal-ui" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="yes" />
+        
+<!--        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />-->
+        
+        <link rel="stylesheet" href="dist/css/mobile-angular-ui-hover.min.css" />
+        <link rel="stylesheet" href="dist/css/mobile-angular-ui-base.min.css" />
+        <link rel="stylesheet" href="dist/css/mobile-angular-ui-desktop.min.css" />
+        
+        <link rel="stylesheet" href="main.css" />
+        
+        
+        
+    </head>
+    <body ng-app="MobileAngularUiExamples" ng-controller="MainController" ui-prevent-touchmove-defaults>
+        
+          <ng-view></ng-view>
+        
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular-route.min.js"></script>
+        <script src="dist/js/mobile-angular-ui.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        
+        <!-- Required to use $touch, $swipe, $drag and $translate services -->
+        <script src="dist/js/mobile-angular-ui.gestures.min.js"></script>
+        <script src="index.js"></script>
+        
+        <script src="views/game/game.controller.js"></script>
+        
+
+  </body>
+</html>
